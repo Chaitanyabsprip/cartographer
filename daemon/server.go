b7491a80335f8b39d1ccdf21586cbf1f324d795f
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-03-02 00:40:14 +0530

    feat: enable daemon requests logging to a file

--- daemon/server.go
+++ daemon/server.go
@@ -1,8 +1,10 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
 	"os"
 	"os/exec"
@@ -13,6 +15,7 @@ import (
 
 	"github.com/chaitanyabsprip/cartographer/cartographer"
 	"github.com/chaitanyabsprip/cartographer/config"
+	"github.com/chaitanyabsprip/cartographer/utils"
 )
 
 func initialise(init chan bool) {
@@ -73,6 +76,30 @@ func Run() {
 	go initialise(initialised)
 	<-initialised
 	e := echo.New()
+	file := utils.OpenLogFile("daemon.log")
+	logger := slog.New(slog.NewJSONHandler(file, nil))
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogStatus:   true,
+		LogURI:      true,
+		LogError:    true,
+		HandleError: true, // forwards error to the global error handler, so it can decide appropriate status code
+		LogValuesFunc: func(_ echo.Context, v middleware.RequestLoggerValues) error {
+			if v.Error == nil {
+				logger.LogAttrs(context.Background(), slog.LevelInfo, "REQUEST",
+					slog.String("uri", v.URI),
+					slog.Int("status", v.Status),
+				)
+			} else {
+				logger.LogAttrs(context.Background(), slog.LevelError, "REQUEST_ERROR",
+					slog.String("uri", v.URI),
+					slog.Int("status", v.Status),
+					slog.String("err", v.Error.Error()),
+				)
+			}
+			return nil
+		},
+	}))
+
 	log.Println("Server started")
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
