131fe2c8bf677f1f88fd13268a54260ab3c1dce6
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-03-06 04:43:39 +0530

    fix: python embedding does not cause segmentation fault

--- embedding/embedding.py.go
+++ embedding/embedding.py.go
@@ -51,23 +51,29 @@ model = SentenceTransformer("%s")
 
 def embed_text(text: str) -> list[float]:
 	clean_text = clean(markdown(text))
-	return self.model.encode([clean_text])[0]
+	return model.encode([clean_text])[0]
 `, transformerName)
 
-	defer p3.Py_Finalize()
 	p3.Py_Initialize()
 	p3.PyRun_SimpleString(pyCodeGo)
 	oModule = p3.PyImport_AddModule("__main__")
-	oModule.IncRef()
 }
 
 func EmbedText(text string) ([]float64, error) {
 	pText := p3.PyUnicode_FromString(text)
 	args := p3.PyTuple_New(1)
 	p3.PyTuple_SetItem(args, 0, pText)
-	embedStringFunc := p3.PyDict_GetItemString(oModule, "embed")
+	embedStringFunc := oModule.GetAttrString("embed_text")
+	if embedStringFunc == nil || !p3.PyCallable_Check(embedStringFunc) {
+		return nil, errors.New("could not find embed_text function in python runtime")
+	}
 	result := embedStringFunc.CallObject(args)
-	defer result.DecRef()
+	if result == nil {
+		p3.PyErr_Print()
+		return nil, errors.New("Failure in calling python function")
+	}
+	// defer pText.DecRef()
+	// defer args.DecRef()
 	return goSliceFromPyList(result)
 }
 
@@ -80,20 +86,23 @@ func EmbedFile(filepath string) ([]float64, error) {
 }
 
 func goSliceFromPyList(pyList *p3.PyObject) ([]float64, error) {
-	if !p3.PyList_Check(pyList) {
-		return nil, errors.New("Value error, expected a list")
-	}
+	defer pyList.DecRef()
+	seq := pyList.GetIter()
+	defer seq.DecRef()
+	tNext := seq.GetAttrString("__next__")
+	defer tNext.DecRef()
 	length := pyList.Length()
 	if length == -1 {
 		return nil, errors.New("Failed to get list size")
 	}
 	arr := make([]float64, length)
 	for i := 0; i < length; i++ {
-		item := p3.PyList_GetItem(pyList, i)
-		if !p3.PyFloat_Check(item) {
+		item := tNext.CallObject(nil)
+		if item == nil && !p3.PyFloat_Check(item) {
 			return nil, errors.New("List contains non float element")
 		}
 		arr[i] = p3.PyFloat_AsDouble(item)
+		item.DecRef()
 	}
 	return arr, nil
 }
